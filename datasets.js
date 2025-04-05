ae14a0a7bb5ce5be8f5f7a39dfcc67ce1a7c8015
gutesbild <62953590+gutesbild@users.noreply.github.com>
2025-04-05 09:50:33 +0200

    Update datasets.js

--- datasets.js
+++ datasets.js
@@ -2,4 +2,5 @@
 const datasets = [
   { file: "kurzeSaetze.json", label: "📍 Kurze Sätze" },
   { file: "Restaurant.json", label: "🍽️ Restaurant" }
+   { file: "pronounsConjugation.json", label: "🤘Verben & Pronomen 101" }
 ];

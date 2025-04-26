04c1dee30d90748a88d774fe6431975bcb99523a
gutesbild <62953590+gutesbild@users.noreply.github.com>
2025-04-26 16:08:44 +0200

    Create OsUndIs.js

---
+++ OsUndIs.js
@@ -0,0 +1,152 @@
+[
+  {
+    "question": "ich",
+    "answer": "εγώ",
+    "phonetic": "eg**o**"
+  },
+  {
+    "question": "du",
+    "answer": "εσύ",
+    "phonetic": "es**i**"
+  },
+  {
+    "question": "er",
+    "answer": "αυτός",
+    "phonetic": "aft**o**s"
+  },
+  {
+    "question": "sie (feminin)",
+    "answer": "αυτή",
+    "phonetic": "aft**i**"
+  },
+  {
+    "question": "wir",
+    "answer": "εμείς",
+    "phonetic": "em**i**s"
+  },
+  {
+    "question": "ihr",
+    "answer": "εσείς",
+    "phonetic": "es**i**s"
+  },
+  {
+    "question": "sie (plural)",
+    "answer": "αυτοί",
+    "phonetic": "aft**i**"
+  },
+  {
+    "question": "leben",
+    "answer": "ζωή",
+    "phonetic": "zo**i**"
+  },
+  {
+    "question": "haus",
+    "answer": "σπίτι",
+    "phonetic": "sp**i**ti"
+  },
+  {
+    "question": "stadt",
+    "answer": "πόλη",
+    "phonetic": "p**o**li"
+  },
+  {
+    "question": "apfel",
+    "answer": "μήλο",
+    "phonetic": "m**i**lo"
+  },
+  {
+    "question": "äpfel",
+    "answer": "μήλα",
+    "phonetic": "m**i**la"
+  },
+  {
+    "question": "sonne",
+    "answer": "ήλιος",
+    "phonetic": "**i**lios"
+  },
+  {
+    "question": "wasser",
+    "answer": "νερό",
+    "phonetic": "ner**o**"
+  },
+  {
+    "question": "ich bin",
+    "answer": "είμαι",
+    "phonetic": "**i**me"
+  },
+  {
+    "question": "ich spreche",
+    "answer": "μιλάω",
+    "phonetic": "mil**a**o"
+  },
+  {
+    "question": "ich verstehe",
+    "answer": "καταλαβαίνω",
+    "phonetic": "katalav**e**no"
+  },
+  {
+    "question": "der mann",
+    "answer": "ο άντρας",
+    "phonetic": "o **a**ndras"
+  },
+  {
+    "question": "die frau",
+    "answer": "η γυναίκα",
+    "phonetic": "i yin**e**ka"
+  },
+  {
+    "question": "die männer",
+    "answer": "οι άντρες",
+    "phonetic": "i **a**ndres"
+  },
+  {
+    "question": "die frauen",
+    "answer": "οι γυναίκες",
+    "phonetic": "i yin**e**kes"
+  },
+  {
+    "question": "das kind",
+    "answer": "το παιδί",
+    "phonetic": "to ped**i**"
+  },
+  {
+    "question": "jetzt",
+    "answer": "τώρα",
+    "phonetic": "t**o**ra"
+  },
+  {
+    "question": "morgen",
+    "answer": "αύριο",
+    "phonetic": "**a**vrio"
+  },
+  {
+    "question": "gestern",
+    "answer": "χθες",
+    "phonetic": "chthes"
+  },
+  {
+    "question": "schön",
+    "answer": "ωραίος",
+    "phonetic": "or**e**os"
+  },
+  {
+    "question": "gut",
+    "answer": "καλός",
+    "phonetic": "kal**o**s"
+  },
+  {
+    "question": "wie",
+    "answer": "πώς",
+    "phonetic": "pos"
+  },
+  {
+    "question": "warum",
+    "answer": "γιατί",
+    "phonetic": "yiat**i**"
+  },
+  {
+    "question": "wo",
+    "answer": "πού",
+    "phonetic": "pou"
+  }
+]
